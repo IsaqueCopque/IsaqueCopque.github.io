b1a5d957b66a02bde1a9496af68f5c1d2b6609f3
IsaqueCopque <isaquecopque@gmail.com>
2026-02-05 22:41:30 -0300

    Updates

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export {default as AboutSection} from "./About";
-export {default as Navbar} from "./Navbar";
-export {default as BannerSection} from "./Banner";
-export {default as ResumeSection} from "./Resume";
-export {default as ContactSection} from "./Contact";
